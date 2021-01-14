9cb3ee72bd88cde953c38e84efbb27c33f6922b1
xoliio <xoliio@gmail.com>
2021-01-14 09:22:23 -0800

    registration and sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController 
+    include CurrentUserConcern 
+
     def create 
         user = User
         .find_by(email: params['user']['email'])
@@ -12,6 +14,18 @@ class SessionsController < ApplicationController
             }
         else
             render json: { status: 401 }
+        end
+    end
+    def logged_in 
+        if @current_user
+            render json: {
+                logged_in: true, 
+                user: @current_user 
+            }
+        else 
+            render json: {
+                logged_in: false
+            }
         end
     end
 end
