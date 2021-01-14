0b03a6e5cbd9df88cec77545e2990e08b9b24696
xoliio <xoliio@gmail.com>
2021-01-14 10:19:02 -0800

    more setup

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,10 +1,10 @@
 class RegistrationsController < ApplicationController 
     def create 
         user = User.create!(
+            name: params['user']['name'], 
             email: params['user']['email'], 
             password: params['user']['password'], 
             password_confirmation: params['user']['password_confirmation'], 
-            name: params['user']['name'], 
             username: params['user']['username'], 
             headline: params['user']['headline']
         )
