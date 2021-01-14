60df7de62a62e4db4a09cf92899d44fa3f6549e0
xoliio <xoliio@gmail.com>
2021-01-14 14:11:24 -0800

    fresh push

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,11 +1,11 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-        origins 'http://localhost:3000'
+        origins 'http://localhost:3001' # front end
         resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
         credentials: true
     end
-    # allow do #domain that this is pushing up to like heroku
-    #     origins 'http://localhost:3000'
+    # allow do # change to firebase domain
+    #     origins 'https://mighty-wildwood-93362.herokuapp.com'
     #     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
     #     credentials: true
     # end
