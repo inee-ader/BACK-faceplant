934d5772a0f1da483124defb02c926138ee5776d
xoliio <xoliio@gmail.com>
2021-01-14 16:51:50 -0800

    fix

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-        origins 'https://peaceful-varahamihira-8367f0.netlify.app' # front end
+        origins 'http://localhost:3001' # front end
         resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
         credentials: true
     end
