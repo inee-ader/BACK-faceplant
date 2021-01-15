74858fb73d70b49306a70852dbf2c3b196e303d7
xoliio <xoliio@gmail.com>
2021-01-14 16:14:46 -0800

    troubleshooting

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-        origins 'http://localhost:3000'
-        resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
-        credentials: true
+        origins 'https://peaceful-varahamihira-8367f0.netlify.app'
+        resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+        # credentials: true
     end
     # allow do #domain that this is pushing up to like heroku
     #     origins 'http://localhost:3000'
