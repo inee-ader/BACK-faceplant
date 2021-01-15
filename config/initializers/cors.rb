694f6e106edc12783dc80873b766c1e23ef366da
xoliio <xoliio@gmail.com>
2021-01-14 16:17:16 -0800

    comma

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
         origins 'https://peaceful-varahamihira-8367f0.netlify.app'
-        resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
-        # credentials: true
+        resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
+        credentials: true
     end
     # allow do #domain that this is pushing up to like heroku
     #     origins 'http://localhost:3000'
