fb2ecf145fc0a7d6ef6542cff981a5c2747c576f
xoliio <xoliio@gmail.com>
2021-01-14 18:42:17 -0800

    actual cors fix

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,5 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-<<<<<<< HEAD
         origins '*'
         resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
         # credentials: true
@@ -13,16 +12,3 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 end
 
 Rails.application.config.hosts = nil
-=======
-        origins 'http://localhost:3001' # front end
-        origins ''
-        resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
-        credentials: true
-    end
-    allow do 
-        origins 'https://peaceful-varahamihira-8367f0.netlify.app/'
-        resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
-        credentials: true
-    end
-end
->>>>>>> origin/main
