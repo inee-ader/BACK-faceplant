5931dd10e89baed2a2874156a8a95d79a240d3d4
xoliio <xoliio@gmail.com>
2021-01-14 19:01:43 -0800

    Allow credential cors header to be set

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-        origins '*'
-        resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
-        # credentials: true
+        origins 'peaceful-varahamihira-8367f0.netlify.app'
+        resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
+        credentials: true
     end
     # allow do # change to firebase domain
     #     origins 'https://mighty-wildwood-93362.herokuapp.com'
