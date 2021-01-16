076f539c4340191bef49c8aabd6a95f009510dc1
xoliio <xoliio@gmail.com>
2021-01-15 20:35:00 -0800

    create method of userplant controller

--- app/controllers/user_plants_controller.rb
+++ app/controllers/user_plants_controller.rb
@@ -26,6 +26,9 @@ class UserPlantsController < ApplicationController
       if @user_plant.save
         format.html { redirect_to @user_plant, notice: 'User plant was successfully created.' }
         format.json { render :show, status: :created, location: @user_plant }
+        render json: {
+          status: :created
+        }
       else
         format.html { render :new }
         format.json { render json: @user_plant.errors, status: :unprocessable_entity }
@@ -40,6 +43,9 @@ class UserPlantsController < ApplicationController
       if @user_plant.update(user_plant_params)
         format.html { redirect_to @user_plant, notice: 'User plant was successfully updated.' }
         format.json { render :show, status: :ok, location: @user_plant }
+        render json: {
+          status: :updated
+        }
       else
         format.html { render :edit }
         format.json { render json: @user_plant.errors, status: :unprocessable_entity }
@@ -63,8 +69,9 @@ class UserPlantsController < ApplicationController
       @user_plant = UserPlant.find(params[:id])
     end
 
+    # do these params makes all these fields REQUIRED?
     # Only allow a list of trusted parameters through.
     def user_plant_params
-      params.require(:user_plant).permit(:user_id, :user_fav, :monograph_id, :common_name, :plant_name, :image_url, :difficulty, :sunlight, :moisture)
+      params.require(:user_plant).permit(:user_id, :user_fav, :monograph_id, :common_name, :plant_name, :personality, :insight, :story_notes, :image_url, :difficulty, :sunlight, :moisture)
     end
 end
