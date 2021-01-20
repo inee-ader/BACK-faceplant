540cead86acc3b44358a02a5ed9001383e776728
xoliio <xoliio@gmail.com>
2021-01-20 15:25:49 -0800

    adds picture uploads

--- app/models/user_plant.rb
+++ app/models/user_plant.rb
@@ -4,6 +4,7 @@ class UserPlant < ApplicationRecord
     # has_many :users, through: :likes
     has_many :likes
     has_many :user_likes, through: :likes, source: :user
+    has_one_attached :image
 
     validates_numericality_of :moisture, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 5
     validates_numericality_of :difficulty, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 5
--- app/serializers/user_plant_serializer.rb
+++ app/serializers/user_plant_serializer.rb
@@ -1,8 +1,18 @@
 class UserPlantSerializer < ActiveModel::Serializer
-  
+  include Rails.application.routes.url_helpers
+
   belongs_to :user 
   has_many :user_likes, through: :likes, source: :user
+  
 
-  attributes :id, :user_id, :user_icon, :user_name, :user_fav, :monograph_id, :common_name, :plant_name, :personality, :insight, :story_notes, :difficulty, :sunlight, :moisture
+  attributes :id, :user_id, :user_icon, :user_name, :image, :user_fav, :monograph_id, :common_name, :plant_name, :personality, :insight, :story_notes, :difficulty, :sunlight, :moisture, :created_at
+
+  def image 
+    if object.image.attached?
+    return rails_blob_path(object.image, only_path: true)
+    else
+      return "no photo"
+    end
+  end
 
 end
