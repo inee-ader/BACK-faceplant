9668576a3db3b7f5cac6e15f065b7e3750d2493a
xoliio <xoliio@gmail.com>
2021-01-22 10:46:35 -0800

    adds another url for cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,7 +5,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     #     credentials: true
     # end
     allow do
-        origins 'http://localhost:3001', 'https://peaceful-varahamihira-8367f0.netlify.app/'
+        origins 'http://localhost:3001', 'https://peaceful-varahamihira-8367f0.netlify.app', 'https://fierce-coast-77229.herokuapp.com'
         resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
         credentials: true
     end
