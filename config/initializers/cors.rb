976339c671d287c1e66cbdb263bd1fc8a6607328
xoliio <xoliio@gmail.com>
2021-01-22 10:06:48 -0800

    before heroku push

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,8 +4,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     #     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
     #     credentials: true
     # end
-      allow do
-        origins 'http://localhost:3001'
+    allow do
+        origins 'http://localhost:3001', 'https://peaceful-varahamihira-8367f0.netlify.app/'
         resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
         credentials: true
     end
