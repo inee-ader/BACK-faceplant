2f18e682bd4a104f897cb1b5f781f034430aac66
xoliio <xoliio@gmail.com>
2021-01-26 12:13:04 -0800

    edited user and plant params

--- app/controllers/user_plants_controller.rb
+++ app/controllers/user_plants_controller.rb
@@ -57,7 +57,7 @@ class UserPlantsController < ApplicationController
     # Only allow a list of trusted parameters through.
     def user_plant_params
 
-      params.permit(:user_id, :user_fav, :user_icon, :user_name, :monograph_id, :common_name, :plant_name, :personality, :insight, :story_notes, :image, :difficulty, :sunlight, :moisture)
+      params.permit(:user_id, :user_fav, :user_icon, :user_name, :common_name, :plant_name, :personality, :insight, :story_notes, :image, :difficulty, :sunlight, :moisture)
 
       # params.require(:user_plant).permit(:user_id, :user_fav, :user_icon, :user_name, :monograph_id, :common_name, :plant_name, :personality, :insight, :story_notes, :image_url, :difficulty, :sunlight, :moisture)
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,7 +35,7 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.require(:user).permit(:name, :username, :headline, :icon)
+        params.require(:user).permit(:name, :username, :icon)
     end
 
 end
